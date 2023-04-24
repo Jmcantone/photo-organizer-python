75b13ffbf998554b110396ff3e3e037648fc2a18
Juanma Cantone <Juan.Cantone@skills-assurance.com>
2023-04-24 15:28:02 +0200

    refactor of the script

--- organize_photos.py
+++ organize_photos.py
@@ -1,25 +1,34 @@
 import os
 import shutil
 
+def get_year_and_month(filename):
+    creation_date = os.path.getctime(filename)
+    year = str(int(creation_date // (365.25 * 24 * 60 * 60)) + 1970)
+    month = str(int((creation_date % (365.25 * 24 * 60 * 60)) // (30.44 * 24 * 60 * 60)) + 1)
+    return year, month
+
+def move_photo_to_folder(src_file, dst_folder):
+    shutil.move(src_file, os.path.join(dst_folder, os.path.basename(src_file)))
+
 def organize_photos_by_year_and_month(directory, threshold):
     # Loop through all the files in the directory
     for file in os.listdir(directory):
-        # Get the creation date of the file
-        creation_date = os.path.getctime(os.path.join(directory, file))
-        year = str(int(creation_date // (365.25 * 24 * 60 * 60)) + 1970)
-        month = str(int((creation_date % (365.25 * 24 * 60 * 60)) // (30.44 * 24 * 60 * 60)) + 1)
+        filename = os.path.join(directory, file)
+        # Get the year and month of the photo
+        year, month = get_year_and_month(filename)
         # Create a folder for the year if it doesn't exist
-        if not os.path.exists(os.path.join(directory, year)):
-            os.makedirs(os.path.join(directory, year))
-        # If the number of photos for the year is greater than the threshold, organize by month
         year_dir = os.path.join(directory, year)
+        if not os.path.exists(year_dir):
+            os.makedirs(year_dir)
+        # If the number of photos for the year is greater than the threshold, organize by month
         year_files = [f for f in os.listdir(year_dir) if os.path.isfile(os.path.join(year_dir, f))]
         if len(year_files) > threshold:
-            if not os.path.exists(os.path.join(year_dir, month)):
-                os.makedirs(os.path.join(year_dir, month))
-            shutil.move(os.path.join(directory, file), os.path.join(year_dir, month, file))
+            month_dir = os.path.join(year_dir, month)
+            if not os.path.exists(month_dir):
+                os.makedirs(month_dir)
+            move_photo_to_folder(filename, month_dir)
         else:
-            shutil.move(os.path.join(directory, file), os.path.join(year_dir, file))
+            move_photo_to_folder(filename, year_dir)
 
 if __name__ == '__main__':
     directory = input("Enter the directory path: ")
